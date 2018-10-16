89489af84cf67ac18c0325a3dfeef4270021f8ca
gwtony <gwtony@gair.local>
2018-10-16 08:38:13 +0800

    modify

--- db/query.go
+++ db/query.go
@@ -6,4 +6,5 @@ const (
 	SUBCLASS_GETBY_CLASSID_SQL = "select * from subclasses where class_id = ?"
 	PRODUCT_GETBY_SUBCLASSID_SQL = "select * from products where subclass_id = ?"
 	PRODUCT_GETBY_SHOPID_SQL = "select products.*, sales.price from products inner join sales on products.id = sales.product_id and sales.shop_id = ?"
+	//select products.*, min(sales.price) from products inner join sales on products.id = sales.product_id and sales.shop_id = 1 group by id;
 )
