077872892ed831f1c5aa797c0ffe2a4ecfcab374
gwtony <gwtony@gair.local>
2018-10-16 08:26:39 +0800

    modify

---
+++ test/rrb_client.sh
@@ -0,0 +1,5 @@
+curl http://localhost:10001/rrbend/class/getall
+curl http://localhost:10001/rrbend/subclass/getall
+curl http://localhost:10001/rrbend/subclass?class_id=1
+curl http://localhost:10001/rrbend/product/subclass?id=1
+curl http://localhost:10001/rrbend/product/shop?id=1
