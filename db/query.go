e8fd4213d77f6abcf63e101d4440b635eb749c37
gwtony <gwtony@gair.local>
2018-10-19 08:29:42 +0800

    modify query debug iu

--- db/query.go
+++ db/query.go
@@ -9,7 +9,7 @@ const (
 
 	CLASS_INSERT_SQL             = "insert into classes (name, description) values (?, ?)"
 	CLASS_UPDATE_SQL             = "update classes set name = ?, description = ? where id = ?"
-	SUBCLASS_INSERT_SQL          = "insert into classes (name, description) values (?, ?)"
+	SUBCLASS_INSERT_SQL          = "insert into subclasses (class_id, name, description) values (?, ?, ?)"
 	SUBCLASS_UPDATE_SQL          = "update subclasses set class_id = ?, name = ?, description = ? where id = ?"
 	PRODUCT_INSERT_SQL           = "insert into products (subclass_id, name, description, display_url) values (?, ?, ?, ?)"
 	PRODUCT_UPDATE_SQL           = "update products set subclass_id = ?, name = ?, description = ?, display_url = ? where id = ?"
--- test/rrb_client.sh
+++ test/rrb_client.sh
@@ -5,8 +5,8 @@ curl http://localhost:10001/rrbend/product/subclass?id=1
 curl http://localhost:10001/rrbend/product/shop?id=1
 
 curl http://localhost:10001/rrbend/class/insert -d '{"name": "class2", "desc": "a class2"}'
-curl http://localhost:10001/rrbend/class/update -d '{"id": 3, "name": "class2", "desc": "a class22"}'
-curl http://localhost:10001/rrbend/subclass/insert -d '{"class_id: "3", "name": "subclass2", "desc": "a subclass2"}'
-curl http://localhost:10001/rrbend/subclass/update -d '{"id": 3, "class_id": 3, "name": "subclass2", "desc": "a subclass22"}'
-curl http://localhost:10001/rrbend/product/insert -d '{"subclass_id": 1, "name": "orange", "desc": "an orange"}'
-curl http://localhost:10001/rrbend/product/update -d '{"id": 3, "subclass_id": 1, "name": "orange", "desc": "an orange2"}'
+curl -i http://localhost:10001/rrbend/class/update -d '{"id": 3, "name": "class2", "desc": "a class22"}'
+curl http://localhost:10001/rrbend/subclass/insert -d '{"class_id": 3, "name": "subclass2", "desc": "a subclass2"}'
+curl http://localhost:10001/rrbend/subclass/update -d '{"id": 5, "class_id": 3, "name": "subclass2", "desc": "a subclass22"}'
+curl http://localhost:10001/rrbend/product/insert -d '{"subclass_id": 1, "name": "orange", "desc": "an orange", "url": "/display_url/product/5.jpg"}'
+curl http://localhost:10001/rrbend/product/update -d '{"id": 5, "subclass_id": 1, "name": "orange", "desc": "an orange2", "url": "/display/product/5.jpg"}'
